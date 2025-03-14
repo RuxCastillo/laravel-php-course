8b3738ff312b49fb5b8d334e4d24d41b0cfe0422
RuxCastillo <rubenldc1412@gmail.com>
2025-03-14 16:01:26 -0600

    siguiendo el curso en el proyecto 1, creando mi primera form en php

--- Project 1 Task List/task-list/routes/web.php
+++ Project 1 Task List/task-list/routes/web.php
@@ -2,7 +2,7 @@
 
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 
 class Task
 {
@@ -18,66 +18,25 @@ class Task
   }
 }
 
-$tasks = [
-  new Task(
-    1,
-    'Buy groceries',
-    'Task 1 description',
-    'Task 1 long description',
-    false,
-    '2023-03-01 12:00:00',
-    '2023-03-01 12:00:00'
-  ),
-  new Task(
-    2,
-    'Sell old stuff',
-    'Task 2 description',
-    null,
-    false,
-    '2023-03-02 12:00:00',
-    '2023-03-02 12:00:00'
-  ),
-  new Task(
-    3,
-    'Learn programming',
-    'Task 3 description',
-    'Task 3 long description',
-    true,
-    '2023-03-03 12:00:00',
-    '2023-03-03 12:00:00'
-  ),
-  new Task(
-    4,
-    'Take dogs for a walk',
-    'Task 4 description',
-    null,
-    false,
-    '2023-03-04 12:00:00',
-    '2023-03-04 12:00:00'
-  ),
-];
-
 Route::get('/', function () {
     return redirect()->route('task.index');
 });
 
-Route::get('/tasks', function () use ($tasks) {
+Route::get('/tasks', function ()  {
     return view('index', [
-        'tasks' => $tasks
+        'tasks' => \App\Models\Task::latest()->get()
     ]);
 })->name('task.index');
 
-Route::get('/tasks/{id}', function ($id) use ($tasks) {
-    $task = collect($tasks)->firstWhere('id', $id);
+Route::view('/tasks/create', 'create')->name('tasks.create');
 
-    if(!$tasks) {
-        abort(404);
-    }
-
-    return view('show', [
-        'task' => $task
-    ]);
+Route::get('/tasks/{id}', function ($id)  {
+    return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
 })->name('task.show');
+
+Route::post('/tasks', function (Request $request) {
+    dd($request->all());
+})->name('task.store');
 
 Route::get('/hello', function () {
     return 'Hello';
